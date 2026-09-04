d795c3671feff4de3da4f18cea11323d546009e1
搜唯网 <291117974@qq.com>
2026-09-04 13:34:01 +0800

    feat: SaaS API V3 的 Java 示例工程（Maven 工程）改造。

---
+++ src/main/java/constant/EsignDemoConfig.java
@@ -0,0 +1,13 @@
+package constant;
+
+public class EsignDemoConfig {
+
+    // 应用ID
+    public static final String EsignAppId = "XXX";
+    // 应用密钥
+    public static final String EsignAppSecret = "XXX";
+    // e签宝接口调用域名（模拟环境）
+    public static final String EsignHost = "https://smlopenapi.esign.cn";
+    // e签宝接口调用域名（正式环境）
+    // public static final String EsignHost = "https://openapi.esign.cn";
+}
